d995240e1b6237c6beb88bde0a7152bc1385a25d
kiminosuizouwotabetai <armanooganiano@yandex.ru>
2025-10-01 14:59:01 +0300

    catclicker

---
+++ Source/CatClicker/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CatClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.catPictureBox = new System.Windows.Forms.PictureBox();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.clickValueLabel = new System.Windows.Forms.Label();
+            this.boostButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.catPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // catPictureBox
+            // 
+            this.catPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.catPictureBox.Image = global::CatClicker.Properties.Resources.caticon;
+            this.catPictureBox.Location = new System.Drawing.Point(89, 97);
+            this.catPictureBox.Name = "catPictureBox";
+            this.catPictureBox.Size = new System.Drawing.Size(200, 200);
+            this.catPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.catPictureBox.TabIndex = 0;
+            this.catPictureBox.TabStop = false;
+            this.catPictureBox.Click += new System.EventHandler(this.catPictureBox_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.scoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(91, 25);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "Счёт: 0";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clickValueLabel
+            // 
+            this.clickValueLabel.AutoSize = true;
+            this.clickValueLabel.BackColor = System.Drawing.Color.Transparent;
+            this.clickValueLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clickValueLabel.Location = new System.Drawing.Point(134, 348);
+            this.clickValueLabel.Name = "clickValueLabel";
+            this.clickValueLabel.Size = new System.Drawing.Size(117, 13);
+            this.clickValueLabel.TabIndex = 2;
+            this.clickValueLabel.Text = "Количество кликов: 1";
+            this.clickValueLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boostButton
+            // 
+            this.boostButton.BackColor = System.Drawing.Color.LightBlue;
+            this.boostButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.boostButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.boostButton.Location = new System.Drawing.Point(141, 303);
+            this.boostButton.Name = "boostButton";
+            this.boostButton.Size = new System.Drawing.Size(100, 40);
+            this.boostButton.TabIndex = 3;
+            this.boostButton.Text = "ТУРБОБУСТ";
+            this.boostButton.UseVisualStyleBackColor = false;
+            this.boostButton.Click += new System.EventHandler(this.boostButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = global::CatClicker.Properties.Resources._1;
+            this.ClientSize = new System.Drawing.Size(374, 439);
+            this.Controls.Add(this.boostButton);
+            this.Controls.Add(this.clickValueLabel);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.catPictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Clicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.catPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox catPictureBox;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label clickValueLabel;
+        private System.Windows.Forms.Button boostButton;
+    }
+}
+
---
+++ Source/CatClicker/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatClicker
+{
+    public partial class Form1 : Form
+    {
+        private int score = 0;
+        private int clickValue = 1;
+        private bool boostActive = false;
+        private int boostTimeLeft = 0;
+        private Timer boostTimer;
+        private Timer redPulseTimer;
+        private bool pulseBright = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeTimers();
+        }
+
+        private void InitializeTimers()
+        {
+            boostTimer = new Timer();
+            boostTimer.Interval = 1000;
+            boostTimer.Tick += BoostTimer_Tick;
+            redPulseTimer = new Timer();
+            redPulseTimer.Interval = 150;
+            redPulseTimer.Tick += RedPulseTimer_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateScoreDisplay();
+        }
+
+        private void catPictureBox_Click(object sender, EventArgs e)
+        {
+            score += clickValue;
+            UpdateScoreDisplay();
+        }
+
+        private void boostButton_Click(object sender, EventArgs e)
+        {
+            if (!boostActive)
+            {
+                ActivateBoost();
+            }
+        }
+
+        private void ActivateBoost()
+        {
+            boostActive = true;
+            clickValue = 2;
+            boostTimeLeft = 10;
+
+            boostTimer.Start();
+            redPulseTimer.Start();
+            boostButton.Enabled = false;
+            boostButton.Text = $"БУСТ: {boostTimeLeft}с";
+        }
+
+        private void RedPulseTimer_Tick(object sender, EventArgs e)
+        {
+            if (pulseBright)
+            {
+                this.BackColor = Color.FromArgb(255, 80, 80);
+                pulseBright = false;
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(200, 50, 50);
+                pulseBright = true;
+            }
+        }
+
+        private void BoostTimer_Tick(object sender, EventArgs e)
+        {
+            boostTimeLeft--;
+            boostButton.Text = $"БУСТ: {boostTimeLeft}с";
+
+            if (boostTimeLeft <= 0)
+            {
+                DeactivateBoost();
+            }
+        }
+
+        private void DeactivateBoost()
+        {
+            boostActive = false;
+            clickValue = 1;
+            boostTimer.Stop();
+            redPulseTimer.Stop();
+            boostButton.Enabled = true;
+            boostButton.Text = "ТУРБОБУСТ";
+            this.BackColor = Color.FromArgb(192, 192, 255);
+        }
+
+        private void UpdateScoreDisplay()
+        {
+            scoreLabel.Text = $"Счёт: {score}";
+            clickValueLabel.Text = $"Количество кликов: {clickValue}";
+        }
+    }
+}
